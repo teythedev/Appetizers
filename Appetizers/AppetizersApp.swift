ecf1e2114071080a5feddf41442915ed127e4ede
tugay emre yucedag <emrethedev@gmail.com>
2023-12-22 06:55:52 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Tugay Emre Yucedag on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
